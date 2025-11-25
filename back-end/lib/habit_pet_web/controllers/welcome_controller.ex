feb56138147690033b66ead85bd46a523375116f
Vagner Santos <santos.vagnerferreira@gmail.com>
2025-11-25 19:38:03 -0300

    Setup python env (#8)
    * deletes elixir project
    
    * setup python hello world

--- back-end/lib/habit_pet_web/controllers/welcome_controller.ex
+++
@@ -1,9 +0,0 @@
-defmodule HabitPetWeb.WelcomeController do
-  use HabitPetWeb, :controller
-
-
-  def index(conn, _params) do
-    conn
-    |> json(%{message: "Welcome to Habit Pet Api"})
-  end
-end
