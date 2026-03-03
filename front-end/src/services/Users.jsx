9718b7ebf5f2a0c460ef200d15e8c132d8abfd6d
matheus-Msantos <matheusmelosantos10@gmail.com>
2026-03-03 19:08:19 -0300

    fix: remove get users api

--- front-end/src/services/Users.jsx
+++ front-end/src/services/Users.jsx
@@ -1,10 +1,5 @@
 import { API } from "./api"
 
-export const GET_USERS = async (payload) => {
-  const response = await API.get('/users/', payload);
-  return response.data;
-}
-
 export const REGISTER_USERS = async (payload) => {
   const response = await API.post('/users/', payload);
   return response.data;
