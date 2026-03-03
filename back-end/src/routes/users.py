7f83559661cadac4ee0dbfb1a7a89f45255c62a7
Vagner <santos.vagnerferreira@gmail.com>
2026-03-03 20:41:16 -0300

    feat(habits): creates PATCH update endpoint

--- back-end/src/routes/users.py
+++ back-end/src/routes/users.py
@@ -2,35 +2,27 @@
 import bcrypt
 from flask import Blueprint, request
 from marshmallow import ValidationError
-from flask_jwt_extended import jwt_required, get_jwt_identity
-import uuid
+from flask_jwt_extended import jwt_required
 
 from src.config.db_config import db
-from src.models.users import UserSchema
+from src.models.users import UserSchema, Users
+from src.decorators import get_user_from_token
 
 users_blueprint = Blueprint('users', __name__)
   
 def get_encrypted_password(password):
   return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-
 @users_blueprint.route('/me', methods=['GET'])
 @jwt_required()
-def get_current_logged_user():
-  from src.models.users import Users
+@get_user_from_token
+def get_current_logged_user(logged_user):
   user_schema = UserSchema()
-
-  user_slug = get_jwt_identity()
-  logged_user = db.session.query(Users).filter_by(slug=uuid.UUID(user_slug)).first_or_404(description="User not found")
-
-  if logged_user:
-    return user_schema.dump(logged_user)
+  return user_schema.dump(logged_user)
 
 
 @users_blueprint.route('/', methods=['POST'])
 def create_user():
-  from src.models.users import Users
-  
   data = request.get_json()
 
   user_schema = UserSchema()
